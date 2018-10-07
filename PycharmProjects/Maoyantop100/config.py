2d6bdcfb6ff8c7f6e143badb0a4e0bcd3560fae1
ytcxxx <18680542683@163.com>
2018-10-07 17:25:04 +0800

    initial

---
+++ PycharmProjects/Maoyantop100/config.py
@@ -0,0 +1,8 @@
+#!/User/anaconda3/python3
+# -*- coding: utf-8 -*-
+# @Time     :   2018/10/7/007 16:42
+# @Author   :   Qingyunke
+
+MONGO_URI = 'localhost'
+MONGO_DB = 'Maoyan_Movies'
+MONGO_COLLECTION = 'Top100'
